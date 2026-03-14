347ac56e99b8f5fe08c7005d4ea66cf4b9937d8f
ratib54 <ratibsway@gmail.com>
2026-03-15 03:14:37 +0600

    add sort method in all tourist place route

--- src/Pages/AllTouristsSpot/AllTouristsSpot.jsx
+++ src/Pages/AllTouristsSpot/AllTouristsSpot.jsx
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 
 const AllTouristsSpot = () => {
+
+
      const placeData = useLoaderData();
-     console.log(placeData)
+
+     const [displayData, setDisplayData] = useState(placeData);
+
+
+     const handleLoweSort = () => {
+          const sorted = placeData.filter(item => item.averageCost <= 10000);
+          setDisplayData(sorted);
+     }
+
+     const handleHighSort = () => {
+          const sorted = placeData.filter(item => item.averageCost >= 10000);
+          setDisplayData(sorted);
+     }
+
+
+
      return (
-          <div className='relative -top-1' >
+          <div className='relative -top-1'>
                <div className="allPlaces flex">
                     <div className="leftSide h-screen bg-primary-0 w-[400px]">
                          <h1 className='text-center  text-[20px] m-12 text-white font-changaOne ' >Find Your Favorite Place</h1>
+
+                         <div className="dropdown dropdown-start ml-12">
+                              <div tabIndex={0} role="button" className="btn m-1">Find your place</div>
+                              <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                                   <button onClick={handleLoweSort}><li><a>Low Cost</a></li></button>
+                                   <button onClick={handleHighSort}><li><a>high cost</a></li></button>
+                              </ul>
+                         </div>
+
+
                     </div>
-                    <div className={`rightSide ${placeData.length <= 3 ? "h-screen" : "h-full"} bg-[#2c505ba9]`}>
+                    <div className={`rightSide ${displayData.length <= 3 ? "h-screen" : "h-full"} bg-[#2c505ba9]`}>
                          <div className='grid grid-cols-3 gap-5 p-10' >
                               {
-                                   placeData.map(places => (
+                                   displayData.map(places => (
                                         <div key={placeData._id} >
                                              <div className="card  border p-6 bg-white">
                                                   <div className="image">
--- src/Pages/PlaceDetails/PlaceDetails.jsx
+++ src/Pages/PlaceDetails/PlaceDetails.jsx
@@ -1,36 +1,45 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-
-const PlaceDetails = () => { 
- const placeDetails = useLoaderData()  ;
- const { image, touristSpotName, country, location, description, averageCost, seasonality, travelTime, totalVisitorsPerYear} = placeDetails ; 
+import { Link, useLoaderData } from 'react-router-dom';
+import { FaBackward } from "react-icons/fa";
+import { GiFireplace } from "react-icons/gi";
+import { FaFlag } from "react-icons/fa";
+import { CiLocationArrow1 } from "react-icons/ci";
+import { FaRegMoneyBillAlt } from "react-icons/fa";
+import { TiWeatherCloudy } from "react-icons/ti";
+import { MdAccessTimeFilled } from "react-icons/md";
+import { IoIosPeople } from "react-icons/io";
+const PlaceDetails = () => {
+ const placeDetails = useLoaderData();
+ const { image, touristSpotName, country, location, description, averageCost, seasonality, travelTime, totalVisitorsPerYear } = placeDetails;
 
  return (
   <div>
-  
-      <button className='btn ml-7 bg-primary-0 text-white font-publicSans' >Back</button>
-       
-       <section className='flex justify-center gap-10 items-center border border-primary-0 max-w-[1000px] mx-auto p-10 mt-8 ' >
-     
-      <div className="image">
-       <img className='h-[600px] w-[1200px] rounded-lg' src={image} alt="place image" />
-      </div> 
 
-      <div className='space-y-6' >
+   <Link to={'/allTouristSpot'} >
+    <button className='btn ml-6 bg-primary-0 text-white font-publicSans' > <FaBackward /> Back</button>
+   </Link>
 
-     <h1 className='text-[#0000009c] text-[16px] font-publicSans' ><span className='text-[#000] text-[17px] font-publicSans font-extrabold uppercase' >Place : </span>{touristSpotName}</h1>
-     <h1 className='text-[#0000009c] text-[16px] font-publicSans' ><span className='text-[#000] text-[17px] font-publicSans font-extrabold uppercase' >Country Name : </span>{country}</h1>
-     <h1 className='text-[#0000009c] text-[16px] font-publicSans' ><span className='text-[#000] text-[17px] font-publicSans font-extrabold uppercase' >Location : </span>{location}</h1>
-     <h1 className='text-[#0000009c] text-[16px] font-publicSans' ><span className='text-[#000] text-[17px] font-publicSans font-extrabold uppercase' >About : </span>{description}</h1> 
-     <h1 className='text-[#0000009c] text-[16px] font-publicSans' ><span className='text-[#000] text-[17px] font-publicSans font-extrabold uppercase' >Cost : </span>{averageCost}</h1>
-     <h1 className='text-[#0000009c] text-[16px] font-publicSans' ><span className='text-[#000] text-[17px] font-publicSans font-extrabold uppercase' >Seasonality : </span>{seasonality}</h1> 
-     <h1 className='text-[#0000009c] text-[16px] font-publicSans' ><span className='text-[#000] text-[17px] font-publicSans font-extrabold uppercase' >Best time to travel : </span>{travelTime}</h1>
-     <h1 className='text-[#0000009c] text-[16px] font-publicSans' ><span className='text-[#000] text-[17px] font-publicSans font-extrabold uppercase' > Total people go there : </span>{totalVisitorsPerYear}</h1>
-    
+   <section className='flex justify-center gap-10 items-center max-w-[1000px] mx-auto p-10 mt-8 ' >
+
+    <div className="image">
+     <img className='h-[600px] w-[1200px] rounded-lg' src={image} alt="place image" />
+    </div>
+
+    <div className='space-y-6' >
+
+     <h1 className='text-[#0000009c] text-[16px] font-publicSans flex gap-3 items-center ' ><span className='text-[#000] text-[17px] font-publicSans font-extrabold uppercase flex gap-3 items-center ' > <GiFireplace /> Place : </span>{touristSpotName}</h1>
+     <h1 className='text-[#0000009c] text-[16px] font-publicSans flex gap-3 items-center' ><span className='text-[#000] text-[17px] font-publicSans font-extrabold uppercase flex gap-3 items-center' > <FaFlag /> Country Name : </span>{country}</h1>
+     <h1 className='text-[#0000009c] text-[16px] font-publicSans flex gap-3 items-center' ><span className='text-[#000] text-[17px] font-publicSans font-extrabold uppercase flex gap-3 items-center' > <CiLocationArrow1 /> Location : </span>{location}</h1>
+     <h1 className='text-[#0000009c] text-[16px] font-publicSans flex gap-7 items-center' ><span className='text-[#000] text-[17px] font-publicSans font-extrabold uppercase flex ' >About </span><span className='w-[400px]' >{description}</span></h1>
+     <h1 className='text-[#0000009c] text-[16px] font-publicSans flex gap-3 items-center' ><span className='text-[#000] text-[17px] font-publicSans font-extrabold uppercase flex gap-3 items-center' > <FaRegMoneyBillAlt /> Cost : </span>{averageCost}</h1>
+     <h1 className='text-[#0000009c] text-[16px] font-publicSans flex gap-3 items-center' ><span className='text-[#000] text-[17px] font-publicSans font-extrabold uppercase flex gap-3 items-center' > <TiWeatherCloudy /> Seasonality : </span>{seasonality}</h1>
+     <h1 className='text-[#0000009c] text-[16px] font-publicSans flex gap-3 items-center' ><span className='text-[#000] text-[17px] font-publicSans font-extrabold uppercase flex gap-3 items-center' > <MdAccessTimeFilled /> Best time to travel : </span>{travelTime}</h1>
+     <h1 className='text-[#0000009c] text-[16px] font-publicSans flex gap-3 items-center' ><span className='text-[#000] text-[17px] font-publicSans font-extrabold uppercase flex gap-3 items-center' > <IoIosPeople /> Total people go there : </span>{totalVisitorsPerYear}</h1>
+
     </div>
 
 
-       </section>
+   </section>
 
   </div>
  );
