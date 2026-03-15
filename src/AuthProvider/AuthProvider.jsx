1976e3a77ef4992e5be50f8b8d537dc3f27fd248
ratib54 <ratibsway@gmail.com>
2026-03-16 01:46:26 +0600

    create user with email and password in firebase Auth

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,11 +1,22 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { createContext } from 'react';
+import auth from '../FirebaseAuth/FirebaseAuth';
 // eslint-disable-next-line react-refresh/only-export-components
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
 
+ 
+ const createUser = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password)
+ }
+
+
+ const userInfo = {
+  createUser
+ }
  return (
-  <AuthContext.Provider >
+  <AuthContext.Provider value={userInfo} >
    {children}
   </AuthContext.Provider>
  );
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,19 +1,88 @@
 import Lottie from 'lottie-react';
-import React from 'react';
+import React, { useContext } from 'react';
 import { FaBackward } from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import lottie from '../../../public/Images/Lottie/signup.json'
+import { AuthContext } from '../../AuthProvider/AuthProvider';
+import toast, { Toaster } from 'react-hot-toast';
+
+
 const Register = () => {
+
+ const { createUser } = useContext(AuthContext);
+ const navigate = useNavigate();
+
+ const handleForm = (e) => {
+  e.preventDefault();
+  const form = e.target;
+
+  const name = form.name.value;
+  const email = form.email.value;
+  const password = form.password.value;
+  const confirmPassword = form.confirmPassword.value;
+
+  console.log(name, email, password, confirmPassword)
+  const user = { name, email, password };
+  console.log(user);
+
+  if (password.length <= 6) {
+   toast.error('Password Length Should Be At Least 6', {
+    style: {
+     backgroundColor: "#000",
+     color : '#fff'
+    }
+   });
+   return;
+  }
+
+
+  createUser(email, password)
+   .then(res => {
+    console.log(res);
+    toast("successfully registered ✅",
+     {
+      style: {
+       backgroundColor: "#000",
+       borderRadius: "10px",
+       width: "700px",
+       color: "#fff"
+      },
+     }
+    )
+    form.reset();
+    navigate('/')
+   })
+   .catch(error => {
+    console.error(error)
+    toast.error(`${error.message}`, {
+     style: {
+      backgroundColor: "#000",
+      borderRadius: "10px",
+      width: "700px",
+      color: "#fff"
+     }
+    })
+   })
+ }
+
+
+ const testToast = () => {
+
+ }
+
+
  return (
   <div className='p-5'>
-
+   <Toaster
+    position='right-top'
+   />
    <Link to={'/'} ><button className='btn bg-primary-0 text-white ' ><FaBackward />back</button></Link>
 
    <h1 className='text-center  text-[40px] m-2 text-primary-0 font-changaOne uppercase ' >Register</h1>
    <section className=' flex justify-center mt-5  '  >
-    <div className="w-[1000px] h-[600px] border bg-primary-0 rounded-xl flex justify-center gap-12 pt-16">
+    <div className="w-[1000px] h-[600px] border bg-primary-0 rounded-xl flex justify-center gap-16 pt-16">
 
-     <form>
+     <form onSubmit={handleForm} >
 
       <div className='space-y-5 ' >
        <div className="name flex items-center gap-4  bg-[#526a6e] p-2 rounded-xl ">
@@ -21,7 +90,8 @@ const Register = () => {
         <input type="text"
          name='name'
          placeholder='Your Name'
-         className='outline-none px-7 py-2 bg-transparent border-b-2 text-white'
+         required
+         className='outline-none px-7 select-none py-2 bg-transparent border-b-2 text-white'
         />
        </div>
 
@@ -29,6 +99,7 @@ const Register = () => {
         <h1 className='text-white font-bold font-publicSans text-[16px] mt-4' >Your email </h1>
         <input type="email"
          name='email'
+         required
          placeholder='email@gmail.com'
          className='outline-none px-7 py-2 bg-transparent border-b-2 text-white'
         />
@@ -38,6 +109,7 @@ const Register = () => {
         <h1 className='text-white font-bold font-publicSans text-[16px] mt-4' > password </h1>
         <input type="password"
          name='password'
+         required
          placeholder='password'
          className='outline-none px-7 py-2 bg-transparent border-b-2 text-white'
         />
@@ -47,17 +119,18 @@ const Register = () => {
         <h1 className='text-white font-bold font-publicSans text-[16px] mt-4' >confirm password </h1>
         <input type="password"
          name='confirmPassword'
+         required
          placeholder='confirm password'
          className='outline-none px-7 py-2 bg-transparent border-b-2 text-white'
         />
-       </div> 
+       </div>
 
-      <button className='bg-white w-[400px] p-3 rounded-xl font-publicSans font-bold uppercase btn '  >Register</button> 
+       <button className='bg-white w-[400px] p-3 rounded-xl font-publicSans font-bold uppercase btn '  >Register</button>
 
 
-      <div>
-       <h1 className='text-white font-publicSans ' >Already have an account ? <Link to={'/login'} className='underline' >Login</Link></h1>
-      </div>
+       <div>
+        <h1 className='text-white font-publicSans ' >Already have an account ? <Link to={'/login'} className='underline' >Login</Link></h1>
+       </div>
 
       </div>
 
@@ -71,6 +144,9 @@ const Register = () => {
 
 
     </div>
+
+    <button onClick={testToast} >toast</button>
+
    </section>
   </div>
  );
